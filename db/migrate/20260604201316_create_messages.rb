5f3c2681761e94ac18a9fc5742c971fcfcdd8e26
Jakob Skjerning <jakob@mentalized.net>
2026-06-06 23:23:44 +0200

    Use UUIDs

--- db/migrate/20260604201316_create_messages.rb
+++ db/migrate/20260604201316_create_messages.rb
@@ -2,7 +2,7 @@
 
 class CreateMessages < ActiveRecord::Migration[8.0]
   def change
-    create_table :messages do |t|
+    create_table :messages, :id => :uuid do |t|
       t.string :slug, :null => false
       t.text :subject, :null => false
       t.text :html_body, :null => false
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2026_06_05_190840) do
-  create_table "deliveries", force: :cascade do |t|
-    t.integer "message_id", null: false
+  create_table "deliveries", id: uuid, force: :cascade do |t|
+    t.string "message_id", limit: 36, null: false
     t.string "recipient_email", null: false
     t.string "variant"
     t.string "email_message_id"
@@ -25,18 +25,8 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_05_190840) do
     t.index ["message_id"], name: "index_deliveries_on_message_id"
   end
 
-  create_table "memberships", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "organization_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["organization_id"], name: "index_memberships_on_organization_id"
-    t.index ["user_id", "organization_id"], name: "index_memberships_on_user_id_and_organization_id", unique: true
-    t.index ["user_id"], name: "index_memberships_on_user_id"
-  end
-
-  create_table "message_variants", force: :cascade do |t|
-    t.integer "message_id", null: false
+  create_table "message_variants", id: uuid, force: :cascade do |t|
+    t.string "message_id", limit: 36, null: false
     t.string "variant", null: false
     t.text "subject", null: false
     t.text "html_body", null: false
@@ -47,7 +37,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_05_190840) do
     t.index ["message_id"], name: "index_message_variants_on_message_id"
   end
 
-  create_table "messages", force: :cascade do |t|
+  create_table "messages", id: uuid, force: :cascade do |t|
     t.string "slug", null: false
     t.text "subject", null: false
     t.text "html_body", null: false
@@ -58,21 +48,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_05_190840) do
     t.index ["slug"], name: "index_messages_on_slug", unique: true
   end
 
-  create_table "organizations", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "organization_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["organization_id"], name: "index_projects_on_organization_id"
-  end
-
-  create_table "providers", force: :cascade do |t|
+  create_table "providers", id: uuid, force: :cascade do |t|
     t.string "provider"
     t.text "api_key"
     t.string "sending_domain"
@@ -81,28 +57,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_06_05_190840) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sessions_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email_address", null: false
-    t.string "password_digest", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email_address"], name: "index_users_on_email_address", unique: true
-  end
-
   add_foreign_key "deliveries", "messages"
-  add_foreign_key "memberships", "organizations"
-  add_foreign_key "memberships", "users"
   add_foreign_key "message_variants", "messages"
-  add_foreign_key "projects", "organizations"
-  add_foreign_key "sessions", "users"
 end
